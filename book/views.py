e27d92f091212394f89e24d2edf1a543fd0f977a
Gonzalo Flores <gonzalo@afachile.cl>
2018-04-02 23:55:11 +0000

    test

--- book/views.py
+++ book/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def books(request):
+    a = "/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas/bookaaaaaaaaaaaas"
+    b = a
     return HttpResponse("<h1>list books</h1>")
     
