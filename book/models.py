e6d9121dd920b660037ee1c323ecb3d0dd799210
Gonzalo Flores Escobar <gonzaloafa@afachile.cl>
2018-04-08 01:00:35 -0300

    add fields in Book and Author

--- book/models.py
+++ book/models.py
@@ -3,15 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):    
-    def __init__(self): 
-        self.tasks = []
-    
-    def is_empty(self):
-        return len(self.tasks) == 0
-    
-    def add_task(self, task):
-        self.tasks.append(task)
-        
-    def first_task(self):
-        return self.tasks[0]
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=60)
+    subtitle = models.CharField(max_length=60)
+    authors = models.ManyToManyField(Author)
+    ref_author = models.CharField(max_length=30)
+    ref_category = models.CharField(max_length=30)
+    ISBN = models.CharField(max_length=13, primary_key=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.ISBN = self.ISBN.replace("-","").strip()
+        super(Book, self).save(*args, **kwargs)
