e710082cfa4703ff6a6936daff726e5810dd20d6
Gonzalo Flores Escobar <gonzaloafa@afachile.cl>
2018-06-02 18:02:55 -0400

    add static in production

--- library/settings.py
+++ library/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
