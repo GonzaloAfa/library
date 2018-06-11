668fe3e4e1f07395f7e64c1998f5f6ec11cca85d
Gonzalo Flores Escobar <gonzaloafa@afachile.cl>
2018-06-11 01:35:41 -0400

    Search and show data by isbn book.

--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from feeds.views import getDataGoogleBook, parserToBook
 
 # Create your views here.
 def books(request):
@@ -8,5 +8,16 @@ def books(request):
 
 # Create your views here.
 def search(request):
-    
-    return render(request, 'book/search.html', {})
+
+    data = {'result': ''}
+
+    if 'isbn' in request.GET:
+        # HTTP GET variables
+        isbn = request.GET.get('isbn', "")
+        query  = {'isbn': isbn}
+        response = getDataGoogleBook(query)
+        data['result'] = parserToBook(query, response['data'])
+
+        print (data)
+
+    return render(request, 'book/search.html', data )
