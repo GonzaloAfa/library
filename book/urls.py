c086c46458d666afb6d893a7ee8dd3864c01cc55
Gonzalo Flores Escobar <gonzaloafa@afachile.cl>
2018-08-27 00:42:01 -0300

    show total book in home

--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,6 @@ from book import views as book
 
 urlpatterns = [
     url(r'^search', book.search, name='search_book'),
-    url(r'^list', book.list),
-    url(r'^add', book.add),
+    url(r'^list', book.list, name='list_book'),
+    url(r'^add', book.add, name='add_book'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from record.models import Record
+from system_auth.models import Workspace
+
 
 @login_required(login_url='/')
 def index(request):
-    data = {}
-    return render(request, 'home/index.html', data )
+
+    ref_workspace= Workspace.objects.filter(ref_profile=request.user.profile, active=True).first()
+    total_records = Record.objects.filter(ref_workspace =ref_workspace ).count()
+
+    # print({'records': records})
+
+    return render(request, 'home/index.html', {'total_records': total_records} )
