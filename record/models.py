2dead62dff4b5a7a7f734353b0dfb830b84449e1
Gonzalo Flores Escobar <gonzaloafa@afachile.cl>
2018-09-01 19:32:20 -0300

    Add ref_owner

--- record/models.py
+++ record/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from book.models import Book
-from system_auth.models import Workspace
+from system_auth.models import Workspace, Profile
 
 
 class Record(models.Model):
@@ -9,6 +9,7 @@ class Record(models.Model):
     status = models.CharField(max_length=30)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    ref_owner = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
     ref_book = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
     ref_workspace = models.ForeignKey(Workspace, on_delete=models.DO_NOTHING, blank = True,null=True)
 
